a94f6ee51db73e5c7d290b3df5260ea7b8859500
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2016-06-06 00:28:53 +0200

    Initial application

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cApp
+ */
+angular.module('cApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
