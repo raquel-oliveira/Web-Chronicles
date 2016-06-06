a0cb4cf74e1d200223570f1f76acd3ba7cac7b44
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2016-06-06 22:55:11 +0200

    show node in the graph visualization

--- app/scripts/controllers/show.js
+++ app/scripts/controllers/show.js
@@ -13,13 +13,13 @@ angular.module('cApp')
     .controller('ShowCtrl', function ($scope, $http) {
       $scope.initGraph = function(story) {
 	var nodes = new vis.DataSet([]);
-	
+
 	var edges = new vis.DataSet([]);
 
 	for (var i = 0; i < story.step.length; ++i) {
 	  if (story.step[i].type === 'multiple_choice') {
 	    nodes.add({ id : parseInt(story.step[i]._id),
-			label : story.step[i]._title });
+			label : story.step[i]._id });
 
 
 	    for (var j = 0; j < story.step[i].nextStep.length; ++j) {
@@ -27,11 +27,11 @@ angular.module('cApp')
 			  to   : parseInt(story.step[i].nextStep[j])  });
 	    }
 	  } else if(story.step[i].type === 'end') {
-	    var color = (story.step[i].win === 'true')?
+	    var color = (story.step[i].win === 'false')?
 		'#882222' : '#228822';
 
 	    nodes.add({ id : parseInt(story.step[i]._id),
-			label : story.step[i]._title,
+			label : story.step[i]._id,
 			color : color,
 		        font: { color : "#FFFFFF" } });
 
@@ -52,13 +52,13 @@ angular.module('cApp')
 
 	var network = new vis.Network(container, data, options);
       };
-      
+
       $scope.initStory = function(story_file) {
  	$http.get(story_file).success(function (data) {
 	  console.log(data.story);
 	  $scope.initGraph(data.story);
 	});
       };
-      
+
       $scope.initStory('stories/minimal_story.xml');
     });
