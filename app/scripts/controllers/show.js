34d0eef7119163fab0a3807091751828abbe8605
Basil DALIÉ <dalie.basil@gmail.com>
2016-06-06 22:17:49 +0200

    Ajout choix de la story (Show)

--- app/scripts/controllers/show.js
+++ app/scripts/controllers/show.js
@@ -11,16 +11,18 @@ angular.module('cApp')
       $httpProvider.interceptors.push('xmlHttpInterceptor');
     })
     .controller('ShowCtrl', function ($scope, $http) {
-      $scope.initGraph = function(story) {
+      $scope.storyFile = null;
+      $scope.stories = null;
+      $scope.network = null;
+      
+      $scope.updateGraph = function(story) {
 	var nodes = new vis.DataSet([]);
-	
 	var edges = new vis.DataSet([]);
 
 	for (var i = 0; i < story.step.length; ++i) {
 	  if (story.step[i].type === 'multiple_choice') {
 	    nodes.add({ id : parseInt(story.step[i]._id),
 			label : story.step[i]._title });
-
 
 	    for (var j = 0; j < story.step[i].nextStep.length; ++j) {
 	      edges.add({ from : parseInt(story.step[i]._id),
@@ -50,15 +52,34 @@ angular.module('cApp')
 
 	var options = { layout : { hierarchical : true } };
 
-	var network = new vis.Network(container, data, options);
+	if ($scope.network === null)
+	  $scope.network = new vis.Network(container, data, options);
+	else
+	  $scope.network.setData(data);
+      
       };
       
       $scope.initStory = function(story_file) {
  	$http.get(story_file).success(function (data) {
 	  console.log(data.story);
-	  $scope.initGraph(data.story);
+	  $scope.updateGraph(data.story);
 	});
       };
       
       $scope.initStory('stories/minimal_story.xml');
+
+      $http.get('stories/stories.xml').success(function (data) {
+	$scope.stories = data.stories.story;
+	console.log($scope.stories);
+	console.log($scope.story);
+      });
+
+      $scope.changeStory = function() {
+	$http.get('stories/' + $scope.storyFile).success(function (data) {
+	  $scope.updateGraph(data.story);
+	});
+
+	console.log($scope.storyFile);
+	
+      }
     });
