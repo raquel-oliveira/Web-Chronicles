cfc98d8c667b5ed0c27bee998b2ca5b07fb28335
Flavian JACQUOT <flavian.jacquot@gmail.com>
2016-06-06 19:42:45 +0200

    implement a simple Rest API for stories - next : loadXML and validator

--- server.js
+++ server.js
@@ -7,17 +7,48 @@ const PORT = 8080;
 
 // App
 const app = express();
-app.get('/', function (req, res) {
+app.get('/stories', function (req, res) {
 
-    res.send('Hello world\n');
+
+    fs.readdir('./app/stories', function(err,data) {
+
+        if(err)
+        {
+            console.log("err");
+            console.log(err);
+            res.statusCode = 404;
+            res.send('error\n');
+        }
+        else {
+            res.statusCode = 200;
+            var resArray = [];
+            data.forEach(function(item) { resArray.push(item.split(".")[0])/* etc etc */ })
+            res.send(resArray)
+        }
+
+    });
+
+});
+
+app.get('/stories/:name', function (req, res) {
+
+
     res.statusCode = 200;
 
+    var name = req.params.name;
 
-
-    fs.readFile('app/index.html', 'utf8',function(err,data) {
-        if(err) console.log("err");
-        console.log(data)
+    fs.readFile('./app/stories/'+name+'.xml', 'utf8',function(err,data) {
+        if(err)
+        {
+            res.statusCode = 404;
+            res.send("story not found");
+        }
+        else {
+            res.statusCode = 200;
+            res.send(data);
+        }
     });
+
 });
 
 app.listen(PORT);
