b8eda0d425e657bbc24e1667dab88aacfbf18c22
Flavian JACQUOT <flavian.jacquot@gmail.com>
2016-06-08 09:48:31 +0200

    improve list stories

--- app/scripts/controllers/play.js
+++ app/scripts/controllers/play.js
@@ -7,126 +7,108 @@
  * # PlayCtrl
  */
 angular.module('cApp')
-      .controller('PlayCtrl', function ($scope, $http) {
-		$scope.stories = null;
-    	$scope.storyName = null;
+    .controller('PlayCtrl', function ($scope, $http) {
+        $scope.stories = null;
+        $scope.storyName = null;
 
 
-		$scope.currentStep = null;
+        $scope.currentStep = null;
 
-		$scope.choices = null;
-		$scope.selected = null;
-		  $scope.answer = "";
-		$scope.nbSteps = 0;
-		$scope.stepId = 0;
+        $scope.choices = null;
+        $scope.selected = null;
+        $scope.answer = "";
+        $scope.nbSteps = 0;
+        $scope.stepId = 0;
 
-		$scope.title = '<Titre>';
-		$scope.description = '<Description>';
-		//$scope.endStatus = "Number of step : " + $scope.nbSteps + "\n" +"Minimum number of step : <not implemented>";
-		$scope.endStatusDisplayed = false;
-		$scope.choose = true;
-		$scope.play = false;
-		$scope.choosed = false;
-
-	$scope.update = function() {
-
-	  $scope.description = $scope.currentStep.description;
-	  if ( $scope.currentStep.win === 'true') {
-	    $scope.endStatusDisplayed = true;
-	  }
-	};
-
-  $scope.startStory = function() {
-    $scope.choose = false;
-    $scope.play = true;
-	$scope.storyName = $scope.selected._file;
-	$scope.initStory($scope.title);
-  };
-
-	$scope.initStory = function(story_file) {
-	    /*$http.get(story_file).success(function (data) {
-			console.log(data.story);
-			$scope.storyName = $scope.stories[0];
-			$scope.story = data.story;
-
-			$scope.play = false;
-
-			$scope.goToStep(0);
-			$scope.update();
-	    });*/
-		$scope.storyName = story_file;
-		$scope.goToStep(0);
-	};
-
-	$scope.goToStep = function(step) {
-		$http.get('stories/'+$scope.storyName+'/step/'+step).success(function (data) {
-			var content = data.content;
-			console.log(data);
-			$scope.currentStep = content;
-			$scope.currentStep.url = 'views/'+content.type+'.html';
-			$scope.choices = content.nextStep;
-			$scope.stepType = content.type;
-
-			//console.log(content.nextStep);
-			$scope.play = true;
+        $scope.title = '<Titre>';
+        $scope.description = '<Description>';
 
 
-			console.log(content);
-			//console.log($scope.choices);
-			++$scope.nbSteps;
-			$scope.update();
-		});
-    };
-		  $scope.change= function(value)
-		  {
-			  console.log('change');
-			  console.log($scope.answer);
-			  $scope.answer = value;
-		  };
+        $scope.endStatusDisplayed = false;
+        $scope.choose = true;
+        $scope.play = false;
+        $scope.choosed = false;
 
-		  $scope.verifyAnswer= function(answer)
-		  {
-			  console.log('verif');
-			  console.log($scope.answer);
-			  console.log(answer);
-			  $http.get('stories/'+$scope.storyName+'/step/'+$scope.currentStep.id+"/reponse/"+answer).then(function (reponse){
-				  if(reponse.status === 200)
-				  {
-					  console.log("good anwser");
-					  console.log(reponse.data);
-					  console.log("good anwser");
-					  $scope.goToStep(reponse.data.answer._stepId);
-				  }
-				  else {
-					  console.log(reponse.data);
-					  console.log("bad anwser");
-					  $scope.hint = 'Hint : '+reponse.data.hint;
-				  }
-			  });
-		  };
+        $scope.update = function () {
 
-	$scope.changeStory = function() {
-	  $scope.endStatusDisplayed = false;
-		console.log($scope.selected);
-		$scope.title = $scope.selected._label;
-	  //$scope.initStory('stories/' + $scope.selected._file);
-	};
+            $scope.description = $scope.currentStep.description;
+            if ($scope.currentStep.win === 'true') {
+                $scope.endStatusDisplayed = true;
+            }
+        };
+
+        $scope.startStory = function () {
+            $scope.choose = false;
+            $scope.play = true;
+            $scope.storyName = $scope.selected._file;
+            $scope.initStory($scope.title);
+        };
+
+        $scope.initStory = function (story_file) {
+
+            $scope.storyName = story_file;
+            $scope.goToStep(0);
+        };
+
+        $scope.goToStep = function (step) {
+            $http.get('stories/' + $scope.storyName + '/step/' + step).success(function (data) {
+                var content = data.content;
+                console.log(data);
+                $scope.currentStep = content;
+                $scope.currentStep.url = 'views/' + content.type + '.html';
+                $scope.choices = content.nextStep;
+                $scope.stepType = content.type;
+
+                //console.log(content.nextStep);
+                $scope.play = true;
 
 
-	//$scope.initStory('stories/minimal_story.xml');
+                console.log(content);
+                //console.log($scope.choices);
+                ++$scope.nbSteps;
+                $scope.update();
+            });
+        };
 
-	var x2js = new X2JS();
-	$http.get('stories/').success(function (data) {
+        $scope.change = function (value) {
+            $scope.answer = value;
+        };
 
-	   var raw = x2js.xml_str2json(data);
-		$scope.stories = raw.stories.story;
-		console.log(data);
-		console.log(raw);
-		console.dir($scope.stories);
-		console.dir($scope.stories[0]);
+        $scope.verifyAnswer = function (answer) {
+            $http.get('stories/' + $scope.storyName + '/step/' + $scope.currentStep.id + "/reponse/" + answer).then(function (reponse) {
+                if (reponse.status === 200) {
+                    console.log("good anwser");
+                    console.log(reponse.data);
+                    console.log("good anwser");
+                    $scope.goToStep(reponse.data.answer._stepId);
+                }
+                else {
+                    console.log(reponse.data);
+                    console.log("bad anwser");
+                    $scope.hint = 'Hint : ' + reponse.data.hint;
+                }
+            });
+        };
 
-	  $scope.selected = $scope.stories[0];
-	});
+        $scope.changeStory = function () {
+            $scope.endStatusDisplayed = false;
+            console.log($scope.selected);
+            $scope.title = $scope.selected._label;
+
+        };
+
+        var x2js = new X2JS();
+        $http.get('stories/').success(function (data) {
+
+            var raw = x2js.xml_str2json(data);
+            $scope.stories = raw.stories.story;
+            console.log(data);
+            console.log(raw);
+            console.dir($scope.stories);
+            console.dir($scope.stories[0]);
+
+            $scope.selected = $scope.stories[0];
+        });
 
 
-  });
+    });
--- app/scripts/controllers/show.js
+++ app/scripts/controllers/show.js
@@ -9,87 +9,103 @@ var x2js = new X2JS();
  * # ShowCtrl
  */
 angular.module('cApp')
-    .config(function ($httpProvider) {
-      $httpProvider.interceptors.push('xmlHttpInterceptor');
-    })
-    .controller('ShowCtrl', function ($scope, $http) {
-      $scope.selected = null;
-      $scope.stories = null;
-      $scope.network = null;
-      $scope.graphStyle = { "height" : "400px"};
+	.config(function ($httpProvider) {
+		$httpProvider.interceptors.push('xmlHttpInterceptor');
+	})
+	.controller('ShowCtrl', function ($scope, $http) {
+		$scope.selected = null;
+		$scope.stories = null;
+		$scope.network = null;
+		$scope.graphStyle = { "height" : "400px"};
 
-      $scope.updateGraph = function(story) {
-        var nodes = new vis.DataSet([]);
-        var edges = new vis.DataSet([]);
+		$scope.updateGraph = function(story) {
+			var nodes = new vis.DataSet([]);
+			var edges = new vis.DataSet([]);
 
-		for (var i = 0; i < story.step.length; ++i) {
-		  if (story.step[i].content.type === 'multiple_choice') {
-			nodes.add({ id : parseInt(story.step[i].content.id),
-				label : story.step[i].content.id });
+			for (var i = 0; i < story.step.length; ++i) {
+				if (story.step[i].content.type === 'multiple_choice') {
+					nodes.add({ id : parseInt(story.step[i].content.id),
+						label : story.step[i].content.id });
 
-			for (var j = 0; j < story.step[i].content.nextStep.length; ++j) {
-			  edges.add({ from : parseInt(story.step[i].content.id),
-				  to   : parseInt(story.step[i].content.nextStep[j])  });
+					for (var j = 0; j < story.step[i].content.nextStep.length; ++j) {
+						edges.add({ from : parseInt(story.step[i].content.id),
+							to   : parseInt(story.step[i].content.nextStep[j])  });
+					}
+
+				} else if(story.step[i].content.type === 'end') {
+					var color = (story.step[i].content.win === 'false')?
+						'#882222' : '#228822';
+
+					nodes.add({ id : parseInt(story.step[i].content.id),
+						label : story.step[i].content.id,
+						color : color,
+						font: { color : "#FFFFFF" } });
+
+				} else if(story.step[i].content.type === 'riddle'){
+					nodes.add({ id : parseInt(story.step[i].content.id),
+						label : story.step[i].content.id });
+
+					if(angular.isUndefined(story.step[i].hiden)){
+						console.log('undefined in update');
+					}
+					else {
+
+						edges.add({ from : parseInt(story.step[i].content.id),
+							to   : parseInt(story.step[i].hiden.answer._stepId)  });
+					}
+
+				} else {
+					nodes.add({ id : parseInt(story.step[i].content.id),
+						label : story.step[i].content.title });
+
+				}
+
 			}
-		  } else if(story.step[i].content.type === 'end') {
-			var color = (story.step[i].content.win === 'false')?
-			'#882222' : '#228822';
-			console.log(story.step[i].content);
-			nodes.add({ id : parseInt(story.step[i].content.id),
-				label : story.step[i].content.id,
-				color : color,
-					font: { color : "#FFFFFF" } });
 
-		  } else {
-			nodes.add({ id : parseInt(story.step[i].content.id),
-				label : story.step[i].content.title });
-		  }
-		}
+			var container = document.getElementById('network-story');
 
-		var container = document.getElementById('network-story');
+			var data = {
+				nodes: nodes,
+				edges: edges
+			};
 
-		var data = {
-		  nodes: nodes,
-		  edges: edges
+			var options = { layout : { hierarchical : true } };
+
+			//if ($scope.network === null) {
+				$scope.network = new vis.Network(container, data, options);
+			//}
+			//else{
+			//$scope.network.setData(data);
+			//}
+
+
 		};
 
-		var options = { layout : { hierarchical : true } };
+		$scope.initStory = function(story_file) {
 
-		if ($scope.network === null) {
-			$scope.network = new vis.Network(container, data, options);
-		}
-		else{
-			$scope.network.setData(data);
-		}
+			$http.get(story_file).success(function (data) {
+				console.log('dok');
+				console.dir(data);
+				console.log('dataok');
+				console.log(data.story);
+				$scope.updateGraph(data.story);
+			});
+		};
 
+		//$scope.initStory('show/stories/minimal_story');
 
-      };
+		$http.get('stories/').success(function (data) {
 
-      $scope.initStory = function(story_file) {
+			var raw = x2js.xml_str2json(data);
 
-		$http.get(story_file).success(function (data) {
-			console.log('dok');
-			console.dir(data);
-			console.log('dataok');
-			console.log(data.story);
-			$scope.updateGraph(data.story);
+			//console.dir(data);
+			$scope.stories = raw.stories.story;
+			$scope.selected = $scope.stories[0];
+		});
+
+		$scope.changeStory = function() {
+			$http.get('show/stories/' + $scope.selected._file).success(function (data) {
+				$scope.updateGraph(data.story);
+			});
+		};
 	});
-      };
-
-      $scope.initStory('show/stories/minimal_story');
-
-      $http.get('stories/').success(function (data) {
-
-		var raw = x2js.xml_str2json(data);
-
-		//console.dir(data);
-		$scope.stories = raw.stories.story;
-		$scope.selected = $scope.stories[0];
-      });
-
-      $scope.changeStory = function() {
-		$http.get('show/stories/' + $scope.selected._file).success(function (data) {
-		$scope.updateGraph(data.story);
-	});
-      };
-    });
