334275a71b73475b2de7542db814bc7a27f9761c
Mathieu Mérino <mathieu.merino@outlook.com>
2016-06-08 13:49:04 +0200

    update afficher titre & desc

--- app/scripts/controllers/play.js
+++ app/scripts/controllers/play.js
@@ -38,7 +38,6 @@ angular.module('cApp')
         /**/
         $scope.changeStory = function () {
             //$scope.endStatusDisplayed = false;
-            //console.log($scope.selected);
             $scope.title = $scope.selected._label; //change to get the title of the story
         };
 
@@ -55,21 +54,13 @@ angular.module('cApp')
         /* Go to the step after click in "next" */
         $scope.goToStep = function (step) {
             $http.get('stories/' + $scope.storyPath + '/step/' + step).success(function (data) {
-                //console.log('stories/' + $scope.storyPath + '/step/' + step)
                 var content = data.content;
-                //console.log(data);
-
+                console.log(content);
                 $scope.currentStep = content;
                 $scope.currentStep.url = 'views/' + content.type + '.html';
-                $scope.choices = content.nextStep;
                 $scope.stepType = content.type;
-
-                //console.log(content.nextStep);
                 $scope.play = true;
-
-
-                console.log(content);
-                //console.log($scope.choices);
+                
                 ++$scope.nbSteps;
                 $scope.update();
             });
@@ -80,32 +71,8 @@ angular.module('cApp')
 
             var raw = x2js.xml_str2json(data);
             $scope.stories = raw.stories.story;
-            console.log(data);
-            console.log(raw);
-            console.dir($scope.stories);
-            console.dir($scope.stories[0]);
 
             $scope.choose = true;
             $scope.selected = $scope.stories[0];
         });
-
-        $scope.change = function (value) {
-            $scope.answer = value;
-        };
-
-        $scope.verifyAnswer = function (answer) {
-            $http.get('stories/' + $scope.storyPath + '/step/' + $scope.currentStep.id + "/reponse/" + answer).then(function (reponse) {
-                if (reponse.status === 200) {
-                    console.log("good anwser");
-                    console.log(reponse.data);
-                    console.log("good anwser");
-                    $scope.goToStep(reponse.data.answer._stepId);
-                }
-                else {
-                    console.log(reponse.data);
-                    console.log("bad anwser");
-                    $scope.hint = 'Hint : ' + reponse.data.hint;
-                }
-            });
-        };
     });
