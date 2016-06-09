602df02ca14452b30f33cfd93321c1f51f0de015
Flavian JACQUOT <flavian.jacquot@gmail.com>
2016-06-09 12:53:54 +0200

    more removing ...

--- app/scripts/shortestpath2.js
+++ app/scripts/shortestpath2.js
@@ -15,8 +15,8 @@ function unpack(from)
     while(from!==0)
     {
         route.push(from);
-        console.log('step');
-        console.log(graph[from]);
+
+
         from = graph[from].visited;
     }
     route.push("0");
@@ -25,7 +25,7 @@ function unpack(from)
 
 function addVertex(item)
 {
-    console.log(item);
+
     var vertex = {
         id: item.content[0].id[0],
         end: false,
@@ -39,27 +39,27 @@ function addVertex(item)
     }
 
     if(typeof item.content[0].nextStep !== 'undefined' && item.content[0].nextStep) {
-        console.log('adding EDGES');
-        console.log(item.content[0].nextStep);
+
+
 
         item.content[0].nextStep.forEach(function(nextStepID) {
-            console.log('adding edge');
+
             vertex.to.push(nextStepID._);
-            console.log(nextStepID._);
+
         });
     }
 
     if(typeof item.hiden !== 'undefined' ) {
-        console.log('adding HIDDEN');
-        console.log(item.hiden[0].answer);
+
+
         item.hiden[0].answer.forEach(function(nextStepID) {
-            console.log('next');
-            console.log(nextStepID.$.stepId);
+
+
             vertex.to.push(nextStepID.$.stepId);
         });
 
     }
-    console.dir(vertex);
+
     graph.push(vertex);
 }
 
@@ -73,56 +73,56 @@ function fillgraph(steps){
 function shortestPath(onlyLength) {
     //graph is arrayVertex[ vertex{id:id, end: true, visited:id to:[ids]}]
     //result is an array
-    console.log('Function');
-    console.log(graph);
+
+
     var iDtoExplore = [];
     iDtoExplore.push(0);
 
-    console.log('exploring');
+
     while (iDtoExplore.length>0) {
         var indexCur = iDtoExplore.shift();
 
-        console.log('exploring');
-        console.log(indexCur);
-        console.log(graph[indexCur]);
+
+
+
         //if (graph[indexCur].visited !== false){
         //  continue;
         // }
-        console.log('win?' );
-        console.log(graph[indexCur].end);
+
+
 
         if(graph[indexCur].end === true)
         {
-            console.log('path -------------------------------------0000000000000000000000000000000000000000 ');
-            console.log(unpack(indexCur));
+
+
 
             if(onlyLength === 'true')
             {
-                console.dir('dist');
-                console.dir('dist');
+
+
                 var len = unpack(indexCur).length+'';
-                console.dir(len);
+
                 return len;
             }
             return unpack(indexCur);
         }
-        console.log('not an end');
+
         graph[indexCur].to.forEach(function(item) {
-            console.log('out');
+
 
             if(graph[item].visited===0)
             {
-                console.log('out');
+
                 graph[item].visited = indexCur;
                 iDtoExplore.push(item);
-                console.log(iDtoExplore.length);
+
             }
         });
-        console.log('ddd');
+
 
     }
 
-    console.log(iDtoExplore.length);
+
     var data = [];
 
     return data;
